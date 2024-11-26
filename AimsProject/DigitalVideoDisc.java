49912ef84e87f0978479aa6d6dd330e0d8deb31e
Nguyen Duc Dai Duong <daiduong2004bn@gmail.com>
2024-11-26 21:48:19 +0700

    class-member

--- AimsProject/DigitalVideoDisc.java
+++ AimsProject/DigitalVideoDisc.java
@@ -4,9 +4,20 @@ public class DigitalVideoDisc {
     private String director;
     private int length;
     private float cost;
+    private static int nbDigitalVideoDics = 0;
+    private int id;
 
+    public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
     public String getTitle(){
         return title;
+    }
+    public void setTitle(String title){
+        this.title = title;
     }
     public String getCategory(){
         return category;
@@ -20,16 +31,21 @@ public class DigitalVideoDisc {
     public float getCost(){
         return cost;
     }
+    
 
     public DigitalVideoDisc(String title){
         super();
         this.title = title;
+        nbDigitalVideoDics++;
+        id = nbDigitalVideoDics;
     }
     public DigitalVideoDisc(String title, String category, float cost){
         super();
         this.title = title;
         this.category = category;
         this.cost = cost;
+        nbDigitalVideoDics++;
+        id = nbDigitalVideoDics;
     }
     public DigitalVideoDisc(String title, String category, String director, float cost){
         super();
@@ -37,6 +53,8 @@ public class DigitalVideoDisc {
         this.category = category;
 		this.director = director;
 		this.cost = cost;
+        nbDigitalVideoDics++;
+        id = nbDigitalVideoDics;
     }
     public DigitalVideoDisc(String title, String category, String director, int length, float cost){
         super();
@@ -45,6 +63,8 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
+        nbDigitalVideoDics++;
+        id = nbDigitalVideoDics;
     }
     public String display(){ // return disc's title and cost
         return "Title: " + title + ", Cost: $" + cost;
